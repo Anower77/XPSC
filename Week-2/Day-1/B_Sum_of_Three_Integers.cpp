cef137b23534575c701b2875f1ec393f0a5bfd46
anower77 <anowerhossain765562@gmail.com>
2024-03-23 00:42:29 +0600

    10th commit

---
+++ Week-2/Day-1/B_Sum_of_Three_Integers.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int k,s; cin>>k>>s;
+    int x,y,z; 
+
+    int count=0;
+    for(int x=0; x<=k; x++)
+    {
+        for(int y=0; y<=k; y++)
+        {
+            int z = s-x-y;
+            if(z>=0 and z<=k) count++;
+        }
+    }
+
+    cout<< count<<'\n';
+
+    return 0;
+}
